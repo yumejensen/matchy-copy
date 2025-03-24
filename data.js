5354fecd194d25cff36692c194eb8c214932e0a9
yumejensen <yumejensen@gmail.com>
2025-03-24 15:35:18 -0500

    datajs

--- data.js
+++ data.js
@@ -19,7 +19,7 @@ animal.species = 'dog';
 // bracket notation, add name your animal's name
 animal['name'] = 'Maruko';
 // give animal noises empty array
-animal['noises'] = []; // why tf isn't this working ??????????????????????
+animal['noises'] = [];
 //print animal to console
 console.log(animal);
 
@@ -33,28 +33,43 @@ var noises = [];
 // give noises first element w/ bracket notation with a string noise
 noises[0] = 'woof';
 // push a noise to the end of noises
-noises.push("bork");
+noises.push('bork'); 
 // unshift noise to the beginning
-noises.unshift("snort");
+noises.unshift('snort');
 // add another element to the end w/ bracket notation (do not hard code)
-noises[noises.length-1] = "whine";
+noises[noises.length] = 'whine';
 
-console.log(noises.length);
-console.log(noises[noises.length-1]);
+console.log(noises.length); 
+console.log(noises[noises.length-1]); 
 console.log(noises);
+
+
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Combining Step 1 and 2 ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+// w/ bracket notation, assign noises property on new noises array
+animal['noises'] = noises;
+// add another noise
+animal.noises.push("huff");
+
+console.log(animal);
 
 
 /* *******************************************************************
  * Step 4 - Review
  *
  * 1. What are the different ways you can access properties on objects?
+ * 
+ * Properties of objects can be accessed with dot or bracket notation. 
  *
  * 2. What are the different ways of accessing elements on arrays?
- *
+ * 
+ * Elements of arrays can be accessed with bracket notation.
+ * The array method pop() changes the original array and also returns the last element.
+ * The array method shift() changes the original array and returns the first element
+ * Arrays can be changed with the push, unshift, and splice methods. 
+ * 
  * *******************************************************************
  */
 
