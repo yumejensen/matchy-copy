c5667ee385ad7285f238d94582c02a5090b3ff01
yumejensen <yumejensen@gmail.com>
2025-03-25 15:00:05 -0500

    replace

--- functions.js
+++ functions.js
@@ -12,14 +12,45 @@
 //////////////////////////////////////////////////////////////////////
 // Step 1 - Search ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+/* Instructions
+Implement a function declaration called `search` that:
+   - Takes a paramater representing an Array of `animals`.
+   - Takes a paramater representing a String, the name of an animal on which to perform a search.
+   - Looks through the `animals` Array, and returns the animal's Object if an animal with that name exists.
+   - Returns `null` if no animal with that name exists
+*/
+
+function search (arrayOfAnimals, nameOfAnimal){
+    // looks through animals array and returns the animal's objects - for loop
+    // IF there is an object with an animal that has that name - if statement
+    for (i = 0; i < arrayOfAnimals.length; i++){
+        if (arrayOfAnimals[i]['name'] === nameOfAnimal){
+            return arrayOfAnimals[i];
+        }
+    }
+}
 
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+/* Instructions
+Write a function declaration called `replace` with a signature of `replace(animals, name, replacement) { //... } ` that:
+   - Takes 3 parameters, an Array of animals, a String representing  the name of an animal on which to perform a search, and an Object that represents the replacement animal.
+   - If an animal with that name exists within the `animals` Array, replace it's entire Object with the replacement Object.
+   - Otherwise do nothing.
+*/
 
-
+function replace (arrayOfAnimals, nameOfAnimal, replacementAnimal){
+    // if nameOfAnimal exists inside arrayOfAnimals, replace that animal with replacementAnimal
+    // for loop to go over array, if statement to replace
+    for (i = 0; i < arrayOfAnimals.length; i++){
+        if (arrayOfAnimals[i]['name'] === nameOfAnimal){
+            // splice method to delete and replace
+        }
+    }
+}
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
